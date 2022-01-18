741c6a3a6c5f777707aab5cd8bfd5d1176e4b4cf
JAYBLA <jayblaenterprises@gmail.com>
2022-01-18 18:37:53 +0300

    refactoring

--- customer/views.py
+++ customer/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
-from django.conf import settings
-from django.core.mail import EmailMessage
-from io import BytesIO
-from django.core.files import File
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import View  # pdf download
-from django.http import  HttpResponse
 from bootstrap_modal_forms.generic import (
   BSModalDeleteView,
   BSModalUpdateView,
@@ -21,7 +15,6 @@ from django.urls import reverse_lazy
 
 from .models import *
 from quotation.models import Quotation
-from .utils import render_to_pdf
 
 from datetime import datetime,timedelta,date
 from .forms import *
@@ -29,25 +22,6 @@ from .forms import *
 base_url = settings.BASE_URL
 
 # Create your views here.
-@login_required()
-def home(request):
-    template = 'home.html'
-    invoices = Invoice.objects.all().order_by('-date')
-    customers = Customer.objects.all().order_by('name')
-    quotations = Quotation.objects.all().order_by('-date')    
-    customer_count = customers.count()
-    invoice_count = invoices.count()
-    quotation_count = quotations.count()
-    context = {			
-        'invoices' : invoices,
-        'customers' : customers,
-        'quotations':quotations,
-        'invoice_count':invoice_count,
-        'customer_count':customer_count,
-        'quotation_count':quotation_count,		
-    }
-    return render(request,template,context)
-
 class CustomerCreateView(LoginRequiredMixin, BSModalCreateView):
     template_name = 'customer-create.html'
     form_class = CustomerForm
