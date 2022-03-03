6fd12f548f730540b66e7dd9812cb0dd87adf931
JAYBLA <jayblaenterprises@gmail.com>
2022-03-03 11:31:06 +0300

    link_callback

--- invoice/utils.py
+++ invoice/utils.py
@@ -6,14 +6,27 @@ import os
 from xhtml2pdf import pisa
 
 def render_to_pdf(template_src, context_dict={}):
-    def fetch_resources(uri, rel):
-        path = os.path.join(settings.STATIC_ROOT, uri.replace(settings.STATIC_URL, ""))
+    def link_callback(uri, rel):
+        # use short variable names
+        sUrl = settings.STATIC_URL      # Typically /static/
+        sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL       # Typically /static/media/
+        mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
 
+        # convert URIs to absolute system paths
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+            raise Exception('media URI must start with %s or %s' % (sUrl, mUrl))
         return path
     template = get_template(template_src)
     html  = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result,link_callback=fetch_resources)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result,link_callback=link_callback)
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
