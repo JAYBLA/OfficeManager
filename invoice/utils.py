afe7053a99de5f10a21222993ed93762d2178d58
JAYBLA <jayblaenterprises@gmail.com>
2022-03-03 11:10:30 +0300

    mahangaiko

--- invoice/utils.py
+++ invoice/utils.py
@@ -8,7 +8,12 @@ def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html  = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result,path=path)
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return None
+    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
+
+    def get_full_path_x(request):
+        full_path = ('http', ('', 's')[request.is_secure()], '://',
+        request.META['HTTP_HOST'], request.path)
+        return ''.join(full_path)
