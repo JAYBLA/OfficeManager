137e87af37b7a648bd69ab4aec31958d5d4f8a69
JAYBLA <jayblaenterprises@gmail.com>
2023-08-01 18:32:51 +0300

    SSL

--- config/settings.py
+++ config/settings.py
@@ -165,9 +165,8 @@ if not DEBUG:
     EMAIL_HOST = config('EMAIL_HOST')
     EMAIL_HOST_USER = config('EMAIL_HOST_USER')
     EMAIL_PORT = config('EMAIL_PORT', cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_SSL', cast=bool, default=True)
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-    ADMINS=[("Jumanne", "jayblagroup@gmail.com")]
+    EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=True)
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')    
 else:
     EMAIL_HOST_USER = 'noreply@jayblagroup.com'
     EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
