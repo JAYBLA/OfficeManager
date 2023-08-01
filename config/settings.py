c6c51941fd2c8c86a8eec0e8c8bd743a2b8dd4c7
JAYBLA <jayblaenterprises@gmail.com>
2023-08-01 18:28:50 +0300

    tls

--- config/settings.py
+++ config/settings.py
@@ -165,7 +165,7 @@ if not DEBUG:
     EMAIL_HOST = config('EMAIL_HOST')
     EMAIL_HOST_USER = config('EMAIL_HOST_USER')
     EMAIL_PORT = config('EMAIL_PORT', cast=int)
-    EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=True)
+    EMAIL_USE_TLS = config('EMAIL_USE_SSL', cast=bool, default=True)
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
     ADMINS=[("Jumanne", "jayblagroup@gmail.com")]
 else:
