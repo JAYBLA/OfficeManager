1d836bd3364ae6c17fdd1626dd68724ef7090c59
JAYBLA <jayblaenterprises@gmail.com>
2025-06-09 11:51:16 +0300

    css fixed

--- invoice/urls.py
+++ invoice/urls.py
@@ -7,13 +7,13 @@ app_name = 'invoice'
 
 urlpatterns = [
     #INVOICE
-    path('invoice/new/', invoice_create, name='new-invoice'),
+    path('invoice/new/', invoice_create, name='create-invoice'),
     path('invoice/new/<int:quotation_id>/', invoice_from_quotation, name='invoice-from-quotation'),    
     path('invoice/<int:id>/', invoice_detail, name='invoice-detail'),
-    path('unpaid-invoices', invoice_unpaid, name='invoice-unpaid'),
-    path('paid-invoices', invoice_paid, name='invoice-paid'),
-    path('invoices', invoice_list, name='invoice_list'),    
-    path('draft-invoices', invoice_draft, name='invoice-draft'),
+    path('unpaid-invoices/', invoice_unpaid, name='invoice-unpaid'),
+    path('paid-invoices/', invoice_paid, name='invoice-paid'),
+    path('invoices/', invoice_list, name='invoice-list'),    
+    path('draft-invoices/', invoice_draft, name='invoice-draft'),
     
     #ITEMS
     path('invoice/<int:id>/item/add/', OrderItemCreateView.as_view(), name='create_item'),
--- receipt/urls.py
+++ receipt/urls.py
@@ -5,9 +5,9 @@ from .views import *
 app_name = 'receipt'
 
 urlpatterns = [
-    path('list/', receipt_list, name='receipt_list'),
-    path('create/', receipt_create, name='receipt_create'),
-    path('update/<int:pk>/', receipt_update, name='receipt_update'),
-    path('delete/<int:pk>/', receipt_delete, name='receipt_delete'),
+    path('list/', receipt_list, name='receipt-list'),
+    path('create/', receipt_create, name='create-receipt'),
+    path('update/<int:pk>/', receipt_update, name='update-receipt'),
+    path('delete/<int:pk>/', receipt_delete, name='delete-receipt'),
     path('receipt/<int:receipt_id>/download/', DownloadableReceipt.as_view(), name='download-receipt'),
 ]
