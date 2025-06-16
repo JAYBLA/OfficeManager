51392033cd6ce9f2d8228e8c29dcad09221004ba
JAYBLA <jayblaenterprises@gmail.com>
2025-06-17 00:56:24 +0300

    exclude next_due on hosting form

--- hosting/forms.py
+++ hosting/forms.py
@@ -4,7 +4,7 @@ from .models import Hosting
 class HostingForm(forms.ModelForm):
     class Meta:
         model = Hosting
-        exclude = ['last_notified']
+        exclude = ['last_notified','next_due']
         widgets = {
             'registration_date': forms.DateInput(attrs={'type': 'date'}),
             'expiring_date': forms.DateInput(attrs={'type': 'date'}),
